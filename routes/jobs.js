dd9546e5eabf60c89f186f482ad335f986ffe268
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2025-02-16 14:20:59 -0300

    created view e alteraçoes melhorias

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const Job = require("../models/Job");
+const { where } = require("sequelize");
 
 // rota de teste
 router.get("/test", (req, res) => {
   res.send("deu certo!");
 });
 
+// detalhe da VAGA  --> view/1, view/2
+router.get("/view/:id", (req, res) =>
+  Job.findOne({
+    where: { id: req.params.id },
+  }).then((job) => {
+    res.render("view", {
+      job,
+    });
+  }).catch(err => console.log(err)));
+
+// form da rota de envio
 router.get("/add", (req, res) => {
   res.render("add");
 });
